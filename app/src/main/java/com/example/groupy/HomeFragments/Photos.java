20f47b3a441ac597a4e7aebf7f40f0a26cb53c0a
anirag2000 <raghaavan3@gmail.com>
2020-05-01 11:37:13 +0530

    Home page

---
+++ app/src/main/java/com/example/groupy/HomeFragments/Photos.java
@@ -0,0 +1,33 @@
+package com.example.groupy.HomeFragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.groupy.R;
+
+
+public class Photos extends Fragment {
+
+
+
+    public Photos() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_photos, container, false);
+    }
+}
